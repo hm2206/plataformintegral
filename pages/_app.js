e68d16a98d0ee9ff5fb18b6a905dab4da7d39f86
Hans <twd2206@gmail.com>
2020-04-09 21:37:24 -0500

    fixed test

--- pages/_app.js
+++ pages/_app.js
@@ -59,7 +59,7 @@ class MyApp extends App {
         <Head>
           <meta charSet="utf-8"></meta>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
-          <meta http-equiv="Content-Security-Policy" content="default-src 'self' *:*; style-src https://*;"></meta>
+          <meta http-equiv="Content-Security-Policy" content="default-src 'self' *:*; style-src https://* http://*;"></meta>
           <title>{app.name} {this.capitalize(titulo)}</title>
           <link rel="shortcut icon" href="/img/logo-unu.png"></link>
           <meta name="theme-color" content="#3063A0"></meta>
@@ -76,7 +76,7 @@ class MyApp extends App {
           <script src="/js/jquery.min.js"></script>
           <script src="/js/popper.min.js"></script>
           <script src="/js/bootstrap.min.js"></script>
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
 
           {/* WPA */}
           <link rel="manifest" href="/manifest.json" />
