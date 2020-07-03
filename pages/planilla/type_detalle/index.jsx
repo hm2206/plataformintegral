eeddf3fc15a861c79948d0ba545ce8b81dc4c0b0
Hans <twd2206@gmail.com>
2020-07-03 11:45:41 -0500

    fixed type_detalle

--- pages/planilla/type_detalle/index.jsx
+++ pages/planilla/type_detalle/index.jsx
@@ -6,6 +6,9 @@ import btoa from 'btoa';
 import {BtnFloat, Body} from '../../../components/Utils';
 import { AUTHENTICATE, AUTH } from '../../../services/auth';
 import { allTypeDetalle } from '../../../storage/actions/typeDetalleActions';
+import { Confirm } from '../../../services/utils';
+import { unujobs } from '../../../services/apis';
+import Swal from 'sweetalert2';
 
 export default class TypeDetalle extends Component {
 
@@ -37,10 +40,24 @@ export default class TypeDetalle extends Component {
         this.setState({ [name]: value })
     }
 
-    getOption(obj, key, index) {
-        let {pathname, query} = Router;
-        query[key] = btoa(obj.id);
-        Router.push({pathname, query});
+    getOption = async (obj, key, index) => {
+        let {pathname, push} = Router;
+        let id = btoa(obj.id);
+        this.setState({ loading: true });
+        switch (key) {
+            case 'edit':
+                await push({ pathname: `${pathname}/${key}`, query: { id } });
+                break;
+            case 'delete':
+                await this.changedState(obj, 0);
+                break;
+            case 'restore':
+                await this.changedState(obj, 1);
+                break;
+            default:
+                break;
+        }
+        this.setState({ loading: false });
     }
 
     handleSearch = () => {
@@ -56,6 +73,21 @@ export default class TypeDetalle extends Component {
         query.page = activePage;
         await push({ pathname, query });
         this.setState({ loading: false });
+    }
+
+    changedState = async (obj, estado = 1) => {
+        let answer = await Confirm("warning", `¿Deseas ${estado ? 'restaurar' : 'desactivar'} el Tip. Detalle "${obj.descripcion}"?`);
+        if (answer) {
+            this.setState({ loading: true });
+            await unujobs.post(`type_detalle/${obj.id}/estado`, { estado })
+            .then(async res => {
+                let { success, message } = res.data;
+                if (!success) throw new Error(message);
+                await Swal.fire({ icon: 'success', text: message });
+                await this.handleSearch();
+            }).catch(err => Swal.fire({ icon: 'error', text: err.message }));
+            this.setState({ loading: false });
+        }
     }
 
     render() {
@@ -98,28 +130,25 @@ export default class TypeDetalle extends Component {
                         options={
                             [
                                 {
-                                    id: 1,
                                     key: "edit",
                                     icon: "fas fa-pencil-alt",
-                                    title: "Editar Cargo",
+                                    title: "Editar Tip. Detalle",
                                     rules: {
                                         key: "estado",
                                         value: 1
                                     }
                                 }, {
-                                    id: 1,
                                     key: "restore",
                                     icon: "fas fa-sync",
-                                    title: "Restaurar Cargo",
+                                    title: "Restaurar Tip. Detalle",
                                     rules: {
                                         key: "estado",
                                         value: 0
                                     }
                                 }, {
-                                    id: 1,
                                     key: "delete",
-                                    icon: "fas fa-trash-alt",
-                                    title: "Eliminar Cargo",
+                                    icon: "fas fa-times",
+                                    title: "Eliminar Tip. Detalle",
                                     rules: {
                                         key: "estado",
                                         value: 1
