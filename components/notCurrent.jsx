6435c9ba1a76bf4719c0e106b659845c0127caf2
Hans <twd2206@gmail.com>
2020-07-14 23:36:50 -0500

    fixed only page

---
+++ components/notCurrent.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+export default ({ my_app }) => {
+    return (
+        <div style={{ background: '#f5f7fa', minHeight: '100vh', fontFamily: 'ProximaNova, sans-serif' }} className="text-center">
+            <div style={{ display: 'flex', width: '100%', height: '100vh', justifyContent: 'center', alignItems: 'center', alignContent: 'center', flexDirection: 'column' }}>
+                
+                <img 
+                    style={{ 
+                        width: "75px", height: "75px",
+                        borderRadius: "0.7em",
+                        border: "4px solid #fff",
+                        boxShadow: "10px 10px rgba(0, 0, 0, .15)",
+                        objectFit: "cover"
+                    }}
+                    src={my_app.icon && my_app.icon_images && my_app.icon_images.icon_200x200 || '/img/base.png'} 
+                    alt="logo"
+                />
+                
+                <h1 style={{ fontWeight: '400', fontSize: '2.3rem', width: "100%" }}>Abriste la app en una nueva pestaña</h1>
+                
+                <div style={{ maxWidth: '700px', marginBottom: '20px', marginTop: '20px', color: '#545454', fontSize: '1.7em', fontFamily: 'ProximaNova, sans-serif', lineHeight: '1.3125' }}>
+                    Para que tengas una mejor experiencia, solo puedes tener una única pestaña.
+                    Cierra esta pestaña y ve a tu sesión activa en otra
+                    pestaña o refresca esta pestaña para convertirla en la principal.
+                </div>
+
+                <span 
+                    style={{ 
+                        marginTop: "20px",
+                        border: "2.5px solid #545454", 
+                        background: 'transparent', 
+                        padding: '0.5em 1.7em', 
+                        borderRadius: '1.5em' ,
+                        fontWeight: '600',
+                        color: '#545454',
+                        letterSpacing: "0.02em",
+                        cursor: "pointer"
+                    }}
+                    onClick={(e) =>history.go(location.href)}
+                >
+                    HACER ESTÁ MI PESTAÑA ACTIVA
+                </span>
+            </div>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,9 @@ import withRedux from 'next-redux-wrapper';
 import initsStore from '../storage/store';
 import Show from '../components/show';
 import Swal from 'sweetalert2';
+import { Beforeunload } from 'react-beforeunload';
+import uid from 'uid';
+import NotCurrent from '../components/notCurrent';
 
 // config router
 Router.onRouteChangeStart = () => {
@@ -89,6 +92,8 @@ class MyApp extends App {
   constructor(props) {
     super(props);
     this.state = {
+      activeTabKey: "",
+      current: true,
       loading: false,
       is_render: false,
       toggle: false,
@@ -104,18 +109,34 @@ class MyApp extends App {
     }
   }
 
+  focusMeTab = async () => {
+    let currentTab = await localStorage.getItem('activeTabKey');
+    if (currentTab != this.state.activeTabKey) {
+      this.setState({ current: false });
+      clearInterval(this.intervalo);
+    }
+  }
+
   componentDidMount = () => {
+    window.onload = async () => {
+      let tab = uid(16);
+      await localStorage.setItem('activeTabKey', tab);
+      await this.setState({ activeTabKey: tab });
+      this.intervalo = setInterval(this.focusMeTab, 1000);
+    };
+
     this.handleScreen();
     window.addEventListener('resize', this.handleScreen);
   }
 
   componentWillUnmount = () => {
     window.removeEventListener('resize', this.handleScreen);
+    clearInterval(this.intervalo);
   }
 
   componentWillReceiveProps = (nextProps) => {
     let { pageProps } = this.props;
-    if (pageProps.pathname != nextProps.pageProps.pathname) this.fireEntity({ render: false, disabled: false });
+    if (nextProps.pathname && pageProps.pathname != nextProps.pageProps.pathname) this.fireEntity({ render: false, disabled: false });
   } 
 
   componentDidCatch = (error, info) => {
@@ -161,115 +182,119 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps, store, isLoggin, _app, is_render, message } = this.props
-    const { loading } = this.state;
+    const { loading, current } = this.state;
 
     return  <Fragment>
-      <Provider store={store}>
-        <Head>
-          <meta charSet="utf-8"></meta>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
-          <title>{_app.name || "Integración"}</title>
-          <link rel="shortcut icon" href={_app.icon_images && _app.icon_images.icon_50x50}></link>
-          <meta name="theme-color" content="#3063A0"></meta>
-          <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,600,700,900" rel="stylesheet" type="text/css" />
-          <link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css" />
-          <link rel="stylesheet" href="/css/all.css" />
-          <link rel="stylesheet" href="/css/buttons.bootstrap4.min.css"></link>
-          <link rel="stylesheet" href="/css/flatpickr.min.css" />
-          <link rel="stylesheet" href="/css/theme.min.css" data-skin="default" />
-          <link rel="stylesheet" href="/css/theme-dark.min.css" data-skin="dark" disabled={true} />
-          <link rel="stylesheet" href="/css/skull.css" />
+      <Beforeunload onBeforeunload={(e) => e.preventDefault()}>
+        <Provider store={store}>
+          <Head>
+            <meta charSet="utf-8"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+            <title>{_app.name || "Integración"}</title>
+            <link rel="shortcut icon" href={_app.icon_images && _app.icon_images.icon_50x50}></link>
+            <meta name="theme-color" content="#3063A0"></meta>
+            <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,600,700,900" rel="stylesheet" type="text/css" />
+            <link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css" />
+            <link rel="stylesheet" href="/css/all.css" />
+            <link rel="stylesheet" href="/css/buttons.bootstrap4.min.css"></link>
+            <link rel="stylesheet" href="/css/flatpickr.min.css" />
+            <link rel="stylesheet" href="/css/theme.min.css" data-skin="default" />
+            <link rel="stylesheet" href="/css/theme-dark.min.css" data-skin="dark" disabled={true} />
+            <link rel="stylesheet" href="/css/skull.css" />
 
-          <script src="/js/jquery.min.js"></script>
-          <script src="/js/popper.min.js"></script>
-          <script src="/js/bootstrap.min.js"></script>
-          {/* css dinamico */}
-          <link rel="stylesheet" href={app.css || ""} />
-          <link rel="stylesheet" href="/css/page_loading.css" />
-          {!isLoggin ?  <link rel="stylesheet" href="/css/no_auth_page_loading.css" /> : null}
-          {/* custom */}
-          <link rel="stylesheet" href="/css/custom.css" />
+            <script src="/js/jquery.min.js"></script>
+            <script src="/js/popper.min.js"></script>
+            <script src="/js/bootstrap.min.js"></script>
+            {/* css dinamico */}
+            <link rel="stylesheet" href={app.css || ""} />
+            <link rel="stylesheet" href="/css/page_loading.css" />
+            {!isLoggin ?  <link rel="stylesheet" href="/css/no_auth_page_loading.css" /> : null}
+            {/* custom */}
+            <link rel="stylesheet" href="/css/custom.css" />
 
-          {/* WPA */}
-          <link rel="manifest" href="/manifest.json" />
-          <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-          <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-          <meta name="theme-color" content="#346cb0"/>
-          <script src="/js/stacked-menu.min.js"></script>
-          <script src="/js/theme.min.js"></script>
-        </Head>
+            {/* WPA */}
+            <link rel="manifest" href="/manifest.json" />
+            <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+            <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+            <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+            <meta name="theme-color" content="#346cb0"/>
+            <script src="/js/stacked-menu.min.js"></script>
+            <script src="/js/theme.min.js"></script>
+          </Head>
 
-        <div id="page_change"></div>
+          <div id="page_change"></div>
 
+          <Show condicion={!is_render}>
+            <LoaderPage message={message}/>
+          </Show>
 
-        <Show condicion={!is_render}>
-          <LoaderPage message={message}/>
-        </Show>
+          <Show condicion={is_render}>
+            <LoadingGlobal display={loading ? 'block' : 'none'}/>
+          </Show>
 
-        <Show condicion={is_render}>
-          <LoadingGlobal display={loading ? 'block' : 'none'}/>
-        </Show>
-        
-        <Show condicion={is_render}>
-          {
-            isLoggin ?
-              <Fragment>
-                <div className="full-layout" id="main">
-                  <div className="gx-app-layout ant-layout ant-layout-has-sider">
-                    <div className="ant-layout">
-                      {/* menu navbar */}
-                      <Navbar onToggle={this.handleToggle} 
-                        my_app={_app} toggle={this.state.toggle} 
-                        setScreenLg={this.handleScreenLg} 
-                        screen_lg={this.state.screen_lg} 
-                        screenX={this.state.screenX}
-                        logout={this.logout}
-                        config_entity={this.state.config_entity}
-                      />
-                      {/* content */}
-                      <div className="gx-layout-content ant-layout-content">
-                        <div className="gx-main-content-wrapper">
-                          <Sidebar 
-                            my_app={_app} 
-                            onToggle={this.handleToggle} 
-                            toggle={this.state.toggle} 
-                            screen_lg={this.state.screen_lg}
-                            logout={this.logout}
-                            refresh={this.state.refresh}
-                          />
-                          <Content screen_lg={this.state.screen_lg}>
-                              <Component {...pageProps} 
-                                fireEntity={this.fireEntity}
-                                toggle={this.state.toggle} 
-                                screenX={this.state.screenX}
-                                my_app={_app}
-                                fireRefreshProfile={this.refreshProfile}
-                                fireLoading={this.fireLoading}
-                                isLoading={loading}
-                              />
-                          </Content>
+          <Show condicion={is_render && !current}>
+            <NotCurrent my_app={_app}/>
+          </Show>
+          
+          <Show condicion={is_render && current}>
+            {
+              isLoggin ?
+                <Fragment>
+                  <div className="full-layout" id="main">
+                    <div className="gx-app-layout ant-layout ant-layout-has-sider">
+                      <div className="ant-layout">
+                        {/* menu navbar */}
+                        <Navbar onToggle={this.handleToggle} 
+                          my_app={_app} toggle={this.state.toggle} 
+                          setScreenLg={this.handleScreenLg} 
+                          screen_lg={this.state.screen_lg} 
+                          screenX={this.state.screenX}
+                          logout={this.logout}
+                          config_entity={this.state.config_entity}
+                        />
+                        {/* content */}
+                        <div className="gx-layout-content ant-layout-content">
+                          <div className="gx-main-content-wrapper">
+                            <Sidebar 
+                              my_app={_app} 
+                              onToggle={this.handleToggle} 
+                              toggle={this.state.toggle} 
+                              screen_lg={this.state.screen_lg}
+                              logout={this.logout}
+                              refresh={this.state.refresh}
+                            />
+                            <Content screen_lg={this.state.screen_lg}>
+                                <Component {...pageProps} 
+                                  fireEntity={this.fireEntity}
+                                  toggle={this.state.toggle} 
+                                  screenX={this.state.screenX}
+                                  my_app={_app}
+                                  fireRefreshProfile={this.refreshProfile}
+                                  fireLoading={this.fireLoading}
+                                  isLoading={loading}
+                                />
+                            </Content>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <div 
-                  className={`aside-backdrop ${this.state.toggle ? 'show' : ''}`}
-                  onClick={this.handleToggle}
+                  <div 
+                    className={`aside-backdrop ${this.state.toggle ? 'show' : ''}`}
+                    onClick={this.handleToggle}
+                  />
+                </Fragment>
+              : <Component {...pageProps} 
+                  my_app={_app} 
+                  fireLoading={this.fireLoading}
+                  isLoading={loading}
                 />
-              </Fragment>
-            : <Component {...pageProps} 
-                my_app={_app} 
-                fireLoading={this.fireLoading}
-                isLoading={loading}
-              />
-          }
-        </Show>
-      
-      </Provider>
-
+            }
+          </Show>
+        
+        </Provider>
+      </Beforeunload>
     </Fragment>
   }
 }
