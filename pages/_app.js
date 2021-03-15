b6c6b5377e82199188282e911003ec5150c3093d
Hans <twd2206@gmail.com>
2021-03-15 10:45:10 -0500

    fixed autologout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import App from 'next/app';
 import env from '../env.json';
 import { ScreenProvider } from '../contexts/ScreenContext';
@@ -16,6 +16,7 @@ import Show from '../components/show';
 import 'react-vertical-timeline-component/style.min.css';
 import "toastify-js/src/toastify.css"
 import { AppProvider } from '../contexts';
+import Cookies from 'js-cookie';
 
 
 // config router
@@ -37,49 +38,60 @@ Router.onRouteChangeError = () => {
 
 
 // app main
-class IntegrationApp extends App {
+const IntegrationApp = ({ app, success, pageProps, Component, auth_token, pathname, query }) => {
 
-	// get initialProps
-	static getInitialProps = async ({ ctx, Component }) => {
-		// calls page's `getInitialProps` and fills `appProps.pageProps`
-		const pageProps = Component.getInitialProps && await Component.getInitialProps(ctx) || {};
-		// obtener el app
-		const { success, app } = await authentication.get('app/me')
-			.then(res => res.data)
-			.catch(err => ({ success: false, app: {} }));
-		// obtener token
-		const auth_token = await AUTH(ctx);
-		// obtener pathname, query
-		let { pathname, query } = ctx;
-		// response 
-		return { pageProps, success, app, auth_token, pathname, query };
+	// propos components
+	let appProps = { success, app, env, pathname, query }
+
+	// recoveri token
+	const recoveryToken = () => {
+		let is_auth_token = Cookies.get('auth_token');
+		let auth_token = localStorage.getItem('auth_token');
+		if (!is_auth_token && auth_token) {
+			Cookies.set('auth_token', auth_token);
+			Router.push('/');
+		}
 	}
 
+	// obtener token
+	useEffect(() => {
+		if (location) recoveryToken();
+	}, [success]);
+	
 	// render
-	render() {
-
-		// props
-		let { app, success, pageProps, Component, auth_token, pathname, query } = this.props;
-		let appProps = { success, app, env, pathname, query }
-
-		return (
-			<ScreenProvider>
-				<AppProvider {...appProps}>
-					{/* auth */}
-					<Show condicion={auth_token}>
-						<AuthProvider>
-							<Component {...pageProps}/>
-						</AuthProvider>
-					</Show>
-					{/* Guest */}
-					<Show condicion={!auth_token}>
+	return (
+		<ScreenProvider>
+			<AppProvider {...appProps}>
+				{/* auth */}
+				<Show condicion={auth_token}>
+					<AuthProvider>
 						<Component {...pageProps}/>
-					</Show>
-				</AppProvider>
-			</ScreenProvider>
-		)
-	}
-};
+					</AuthProvider>
+				</Show>
+				{/* Guest */}
+				<Show condicion={!auth_token}>
+					<Component {...pageProps}/>
+				</Show>
+			</AppProvider>
+		</ScreenProvider>
+	)
+}
+
+// static get initial props
+IntegrationApp.getInitialProps = async ({ ctx, Component }) => {
+	// calls page's `getInitialProps` and fills `appProps.pageProps`
+	const pageProps = Component.getInitialProps && await Component.getInitialProps(ctx) || {};
+	// obtener el app
+	const { success, app } = await authentication.get('app/me')
+		.then(res => res.data)
+		.catch(err => ({ success: false, app: {} }));
+	// obtener token
+	const auth_token = await AUTH(ctx);
+	// obtener pathname, query
+	let { pathname, query } = ctx;
+	// response 
+	return { pageProps, success, app, auth_token, pathname, query };
+}
 
 // exportar
 export default IntegrationApp;
