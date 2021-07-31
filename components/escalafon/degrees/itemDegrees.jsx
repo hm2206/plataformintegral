b8c4feac12e06cefbb5b7126a3a57d813a04925d
Hans <twd2206@gmail.com>
2021-07-31 06:05:12 -0500

    fix degrees

--- components/escalafon/degrees/itemDegrees.jsx
+++ components/escalafon/degrees/itemDegrees.jsx
@@ -17,8 +17,9 @@ const ItemDegrees = ({ degree, onUpdate = null, onDelete = null }) => {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="mb-2"><b>Tipo. Grado: </b> {degree?.type_degree?.name}</div>
-                        <div className="mb-2"><b>Institución: </b> {moment(degree?.date_start).format('DD/MM/YYYY')}</div>
-                        <div className="mb-2"><b>N° Documento: </b> {degree?.type_degree?.name}</div>
+                        <div className="mb-2"><b>Institución: </b> {degree?.institution}</div>
+                        <div className="mb-2"><b>N° Documento: </b> {degree?.document_number}</div>
+                        <div className="mb-2"><b>Fecha: </b> {moment(degree?.date).format('DD/MM/YYYY')}</div>
                         <div className="mb-2"><b>Descripción: </b> {degree?.description}</div>
                     </div>
                 </div>
