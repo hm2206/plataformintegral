5c2b82f07db677aceaf3631c0f8b2f1d2a91d4b6
Hans <twd2206@gmail.com>
2021-08-13 10:33:21 -0500

    add create person => escalafon

--- pages/escalafon/trabajador/create.jsx
+++ pages/escalafon/trabajador/create.jsx
@@ -11,6 +11,7 @@ import { SelectAfp, SelectBanco } from '../../../components/select/escalafon';
 import BoardSimple from '../../../components/boardSimple'
 import ContentControl from '../../../components/contentControl';
 import Router from 'next/router';
+import CreatePerson from '../../../components/authentication/person/createPerson'
 
 
 const CreateWork = ({ pathname, query }) => {
@@ -89,11 +90,19 @@ const CreateWork = ({ pathname, query }) => {
                                             <div className="row">
                                                 <Show condicion={!isPerson}>
                                                     <div className="col-md-4">
+                                                        <Button color="green"
+                                                            basic
+                                                            disabled={current_loading}
+                                                            onClick={(e) => setOption("CREATE")}
+                                                        >
+                                                            <i className="fas fa-plus"></i> Nueva Persona
+                                                        </Button>
+
                                                         <Button
                                                             disabled={current_loading}
                                                             onClick={(e) => setOption("ASSIGN")}
                                                         >
-                                                            <i className="fas fa-plus"></i> Asignar
+                                                            <i className="fas fa-arrow-down"></i> Asignar
                                                         </Button>
                                                     </div>
                                                 </Show>
@@ -131,6 +140,14 @@ const CreateWork = ({ pathname, query }) => {
                                                     </div>
 
                                                     <div className="col-md-4 mb-3">
+                                                        <Button color="red"
+                                                            basic
+                                                            onClick={(e) => setPerson({})}
+                                                            disabled={current_loading}
+                                                        >
+                                                            <i className="fas fa-times"></i> Cancelar 
+                                                        </Button>
+
                                                         <Button
                                                             onClick={(e) => setOption("ASSIGN")}
                                                             disabled={current_loading}
@@ -275,6 +292,13 @@ const CreateWork = ({ pathname, query }) => {
                         <AssignPerson
                             getAdd={handleAdd}
                             isClose={(e) => setOption("")}
+                        />
+                    </Show>
+
+                    <Show condicion={option == 'CREATE'}>
+                        <CreatePerson 
+                            onSave={handleAdd}
+                            onClose={() => setOption('')}
                         />
                     </Show>
                 </div>
