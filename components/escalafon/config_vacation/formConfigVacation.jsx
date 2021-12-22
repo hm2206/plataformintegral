bdb946ead6c38a23f075ecfd31d8ff2687580991
Hans <twd2206@gmail.com>
2021-12-22 07:27:53 -0500

    add observation

--- components/escalafon/config_vacation/formConfigVacation.jsx
+++ components/escalafon/config_vacation/formConfigVacation.jsx
@@ -63,6 +63,17 @@ const FormConfigVacation = ({ children, form = {}, errors = {}, className = null
                     <label htmlFor="">{errors?.date_over?.[0] || ""}</label>
                 </Form.Field>
 
+                <Form.Field className="col-md-12 mb-3" error={errors?.observation?.[0] ? true : false}>
+                    <label htmlFor="">Observación</label>
+                    <textarea rows={5}
+                        name="observation"
+                        readOnly={readOnly.includes('observation') || disabled}
+                        value={form?.observation || ""}
+                        onChange={(e) => handleChange(e,  e.target)}
+                    />
+                    <label htmlFor="">{errors?.observation?.[0] || ""}</label>
+                </Form.Field>
+
                 {children || null}
             </div>
         </Form>
