7b580775fe43bf8bbb3142d09b5d8149596c1541
Hans Medina <twd2206@gmail.com>
2022-03-13 07:27:39 -0500

    fix year datos

--- components/planilla/work/report-renta.jsx
+++ components/planilla/work/report-renta.jsx
@@ -2,10 +2,20 @@ import Modal from "../../modal";
 import { Icon, Button } from "semantic-ui-react";
 import { SelectListYearToWork } from "../../select/micro-planilla";
 import { useState } from "react";
+import { microPlanilla } from "../../../services/apis";
+import urljoin from "url-join";
 
 export const ReportRenta = ({ isOpen = true, onClose = null, work = {} }) => {
 
   const [year, setYear] = useState();
+
+  const handleLink = () => {
+    const a = document.createElement("a")
+    const url = urljoin(microPlanilla.path, `works/${work?.id}/historyResume/${year}`)
+    a.href = url;
+    a.target = '__blank';
+    a.click();
+  }
 
   return (
     <Modal show={isOpen} 
@@ -20,7 +30,7 @@ export const ReportRenta = ({ isOpen = true, onClose = null, work = {} }) => {
           onChange={(e, obj) => setYear(obj.value)}
         />
         <div className="mt-3 text-right">
-          <Button color="red">
+          <Button color="red" onClick={handleLink}>
             <i className="far fa-file-pdf"></i> Generar Reporte
           </Button>
         </div>
